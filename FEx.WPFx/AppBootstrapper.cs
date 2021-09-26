f3e07b879052847fd064ceb4c2dd909467a530de
Jan Abramek <abramek.j@gmail.com>
2021-09-26 16:37:34 +0200

    p1

--- FEx.WPFx/AppBootstrapper.cs
+++ FEx.WPFx/AppBootstrapper.cs
@@ -3,12 +3,14 @@ using FEx.DependencyInjection;
 using FEx.Utilities;
 using Microsoft.Extensions.DependencyInjection;
 using System;
+using System.Diagnostics;
 using System.Windows;
 using System.Windows.Threading;
+using WpfBindingErrors;
 
 namespace FEx.WPFx
 {
-    public class AppBootstrapper : Application
+    public abstract class AppBootstrapper : Application
     {
         protected AppBootstrapper()
         {
@@ -16,12 +18,7 @@ namespace FEx.WPFx
             DispatcherUnhandledException += OnAppDispatcherUnhandledException;
         }
 
-        protected virtual void ConfigureServiceProvider()
-        {
-            FExIoCProvider.IoCProvider.ConfigureServiceProvider(ConfigureServices);
-        }
-
-        protected virtual IServiceCollection ConfigureServices(IServiceCollection services) => services;
+        protected abstract void ComponentInitialize();
 
         private void OnAppDispatcherUnhandledException(object sender, DispatcherUnhandledExceptionEventArgs e)
         {
@@ -57,6 +54,118 @@ namespace FEx.WPFx
             }
 
             return registry;
+        }
+
+        /// <summary>
+        /// Raises the <see cref="E:System.Windows.Application.Startup" /> event.
+        /// </summary>
+        /// <param name="e">A <see cref="T:System.Windows.StartupEventArgs" /> that contains the event data.</param>
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            try
+            {
+                EnsureSingleInstance();
+                Guid s = LogToHub("Initializing app");
+                OnConstruction(e);
+                Initialize();
+                RemoveLog(s);
+                s = LogToHub("Preparing app");
+                BeforeStartup(e);
+                RemoveLog(s);
+                s = LogToHub("Initializing app components");
+                Helper.ConfigureServiceProvider(services =>
+                    ConfigureServices(services
+                        .AddSingleton(DispatcherContextExecutor.Instance)
+                        .AddSingleton<IUIContextExecutor>(DispatcherContextExecutor.Instance)
+                        .AddSingleton(DefaultUIContextExecutor.Instance)));
+                AfterServicesContainerBuild();
+                RemoveLog(s);
+                LogToHub("Window creation");
+                base.OnStartup(e);
+                WpfCommon.SetGlobalUIContextExecutor();
+                s = LogToHub("Finalizing startup");
+                AfterStartup(e);
+                RemoveLog(s);
+            }
+            catch (Exception ex)
+            {
+                HandleCriticalException(ex);
+                HasBeenInitialized = false;
+            }
+        }
+
+        private void Initialize()
+        {
+            ComponentInitialize();
+            HasBeenInitialized = true;
+        }
+
+        private void EnsureSingleInstance()
+        {
+            Guid s = LogToHub("Checking duplicated instances");
+            if (!CommonServicesModule.EnsureSingleInstance())
+            {
+                if (WpfCommon.ShowMessageBox(
+                    $"{CommonServicesModule.ApplicationName} is already running.{Environment.NewLine}Do you want to close it?",
+                    buttons: MessageBoxButton.YesNo) == MessageBoxResult.No)
+                {
+                    Environment.Exit(0);
+                }
+                else
+                {
+                    foreach (int pid in CommonServicesModule.GetOtherInstances())
+                    {
+                        using (var p = Process.GetProcessById(pid))
+                        {
+                            p.Kill();
+                        }
+                    }
+                }
+            }
+            RemoveLog(s);
+        }
+
+        protected virtual void ConfigureServiceProvider(IServiceCollection services = null, bool buildProvider = true)
+        {
+            IServiceProvider provider = FExIoCProvider.IoCProvider.ConfigureServiceProvider(ConfigureServices, services, buildProvider);
+
+            if (buildProvider)
+            {
+                AfterServicesContainerBuild(provider);
+            }
+        }
+
+        protected virtual IServiceCollection ConfigureServices(IServiceCollection services) => services;
+
+        protected virtual void AfterServicesContainerBuild(IServiceProvider provider)
+        {
+        }
+
+        protected virtual void BeforeStartup(StartupEventArgs e)
+        {
+            WPFUtilities.OverrideFormattingOnUI();
+
+            BeforeInitializationCheck();
+
+            ExitIfInitializationHasFailed();
+        }
+
+        protected virtual void ExitIfInitializationHasFailed()
+        {
+            if (HasInitializationFailed())
+            {
+                Environment.Exit(1);
+            }
+        }
+
+        protected virtual bool HasInitializationFailed()
+        {
+            return !HasBeenInitialized || ExceptionHandler.LastException != null;
+        }
+
+        protected virtual void AfterStartup(StartupEventArgs e)
+        {
+            BindingExceptionThrower.Attach();
         }
     }
 }
---
+++ FEx.WPFx/WPFUtilities.cs
@@ -0,0 +1,22 @@
+﻿using System.Globalization;
+using System.Windows;
+using System.Windows.Markup;
+
+namespace FEx.WPFx
+{
+    public class WPFUtilities
+    {
+        /// <summary>
+        /// Overrides formatting on UI.
+        /// </summary>
+        /// <param name="culture">The culture to use. If <c>null</c>, <see cref="System.Globalization.CultureInfo.CurrentCulture"/> is used.</param>
+        public static void OverrideFormattingOnUI(CultureInfo culture = null)
+        {
+            culture ??= CultureInfo.CurrentCulture;
+            FrameworkElement.LanguageProperty.OverrideMetadata(
+                typeof(FrameworkElement),
+                new FrameworkPropertyMetadata(
+                    XmlLanguage.GetLanguage(culture.IetfLanguageTag)));
+        }
+    }
+}

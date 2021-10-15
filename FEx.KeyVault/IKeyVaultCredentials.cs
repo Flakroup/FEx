e48d890de36c44fc60f742c6b20e4f6cc02a6a2f
Jan Abramek <abramek.j@gmail.com>
2021-10-15 12:20:28 +0200

    KeyVault integration

---
+++ FEx.KeyVault/IKeyVaultCredentials.cs
@@ -0,0 +1,19 @@
+﻿namespace FEx.KeyVault
+{
+    public interface IKeyVaultCredentials
+    {
+        string AzureADTenantId { get; }
+        string AzureADClientId { get; }
+        string KeyVaultName { get; }
+    }
+
+    public interface IKeyVaultByCertCredentials : IKeyVaultCredentials
+    {
+        string AzureADCertThumbprint { get; }
+    }
+
+    public interface IKeyVaultByClientSecretCredentials : IKeyVaultCredentials
+    {
+        string AzureADClientSecret { get; }
+    }
+}

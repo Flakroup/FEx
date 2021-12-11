b1e220a46700666338fa5d3851634d668d9595e7
Jan Abramek <abramek.j@gmail.com>
2021-12-11 11:29:52 +0100

    p2

--- FEx.WPFx/AppBootstrapper.cs
+++ FEx.WPFx/AppBootstrapper.cs
@@ -3,7 +3,6 @@ using FEx.DependencyInjection;
 using FEx.Utilities;
 using Microsoft.Extensions.DependencyInjection;
 using System;
-using System.Diagnostics;
 using System.Windows;
 using System.Windows.Threading;
 using WpfBindingErrors;
@@ -19,24 +18,6 @@ namespace FEx.WPFx
         }
 
         protected abstract void ComponentInitialize();
-
-        private void OnAppDispatcherUnhandledException(object sender, DispatcherUnhandledExceptionEventArgs e)
-        {
-            if (e.Exception != null)
-            {
-                HandleAppException(e.Exception);
-            }
-
-            e.Handled = true;
-        }
-
-        private void AppDomainUnhandledException(object sender, UnhandledExceptionEventArgs e)
-        {
-            if (e.ExceptionObject is Exception exception)
-            {
-                HandleAppException(exception);
-            }
-        }
 
         protected virtual void HandleAppException(Exception exception)
         {
@@ -56,75 +37,6 @@ namespace FEx.WPFx
             return registry;
         }
 
-        /// <summary>
-        /// Raises the <see cref="E:System.Windows.Application.Startup" /> event.
-        /// </summary>
-        /// <param name="e">A <see cref="T:System.Windows.StartupEventArgs" /> that contains the event data.</param>
-        protected override void OnStartup(StartupEventArgs e)
-        {
-            try
-            {
-                EnsureSingleInstance();
-                Guid s = LogToHub("Initializing app");
-                OnConstruction(e);
-                Initialize();
-                RemoveLog(s);
-                s = LogToHub("Preparing app");
-                BeforeStartup(e);
-                RemoveLog(s);
-                s = LogToHub("Initializing app components");
-                Helper.ConfigureServiceProvider(services =>
-                    ConfigureServices(services
-                        .AddSingleton(DispatcherContextExecutor.Instance)
-                        .AddSingleton<IUIContextExecutor>(DispatcherContextExecutor.Instance)
-                        .AddSingleton(DefaultUIContextExecutor.Instance)));
-                AfterServicesContainerBuild();
-                RemoveLog(s);
-                LogToHub("Window creation");
-                base.OnStartup(e);
-                WpfCommon.SetGlobalUIContextExecutor();
-                s = LogToHub("Finalizing startup");
-                AfterStartup(e);
-                RemoveLog(s);
-            }
-            catch (Exception ex)
-            {
-                HandleCriticalException(ex);
-                HasBeenInitialized = false;
-            }
-        }
-
-        private void Initialize()
-        {
-            ComponentInitialize();
-            HasBeenInitialized = true;
-        }
-
-        private void EnsureSingleInstance()
-        {
-            Guid s = LogToHub("Checking duplicated instances");
-            if (!CommonServicesModule.EnsureSingleInstance())
-            {
-                if (WpfCommon.ShowMessageBox(
-                    $"{CommonServicesModule.ApplicationName} is already running.{Environment.NewLine}Do you want to close it?",
-                    buttons: MessageBoxButton.YesNo) == MessageBoxResult.No)
-                {
-                    Environment.Exit(0);
-                }
-                else
-                {
-                    foreach (int pid in CommonServicesModule.GetOtherInstances())
-                    {
-                        using (var p = Process.GetProcessById(pid))
-                        {
-                            p.Kill();
-                        }
-                    }
-                }
-            }
-            RemoveLog(s);
-        }
-
         protected virtual void ConfigureServiceProvider(IServiceCollection services = null, bool buildProvider = true)
         {
             IServiceProvider provider = FExIoCProvider.IoCProvider.ConfigureServiceProvider(ConfigureServices, services, buildProvider);
@@ -135,7 +47,10 @@ namespace FEx.WPFx
             }
         }
 
-        protected virtual IServiceCollection ConfigureServices(IServiceCollection services) => services;
+        protected virtual IServiceCollection ConfigureServices(IServiceCollection services)
+        {
+            return services;
+        }
 
         protected virtual void AfterServicesContainerBuild(IServiceProvider provider)
         {
@@ -145,7 +60,7 @@ namespace FEx.WPFx
         {
             WPFUtilities.OverrideFormattingOnUI();
 
-            BeforeInitializationCheck();
+            //BeforeInitializationCheck();
 
             ExitIfInitializationHasFailed();
         }
@@ -160,12 +75,100 @@ namespace FEx.WPFx
 
         protected virtual bool HasInitializationFailed()
         {
-            return !HasBeenInitialized || ExceptionHandler.LastException != null;
+            //return !HasBeenInitialized || ExceptionHandler.LastException != null;
+            return false;
         }
 
         protected virtual void AfterStartup(StartupEventArgs e)
         {
             BindingExceptionThrower.Attach();
+        }
+
+        /// <summary>
+        ///     Raises the <see cref="E:System.Windows.Application.Startup" /> event.
+        /// </summary>
+        /// <param name="e">A <see cref="T:System.Windows.StartupEventArgs" /> that contains the event data.</param>
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            try
+            {
+                EnsureSingleInstance();
+                //Guid s = LogToHub("Initializing app");
+                //OnConstruction(e);
+                Initialize();
+                //RemoveLog(s);
+                //s = LogToHub("Preparing app");
+                //BeforeStartup(e);
+                //RemoveLog(s);
+                //s = LogToHub("Initializing app components");
+                //Helper.ConfigureServiceProvider(services =>
+                //    ConfigureServices(services
+                //        .AddSingleton(DispatcherContextExecutor.Instance)
+                //        .AddSingleton<IUIContextExecutor>(DispatcherContextExecutor.Instance)
+                //        .AddSingleton(DefaultUIContextExecutor.Instance)));
+                //AfterServicesContainerBuild();
+                //RemoveLog(s);
+                //LogToHub("Window creation");
+                //base.OnStartup(e);
+                //WpfCommon.SetGlobalUIContextExecutor();
+                //s = LogToHub("Finalizing startup");
+                //AfterStartup(e);
+                //RemoveLog(s);
+            }
+            catch (Exception ex)
+            {
+                //HandleCriticalException(ex);
+                //HasBeenInitialized = false;
+            }
+        }
+
+        private void OnAppDispatcherUnhandledException(object sender, DispatcherUnhandledExceptionEventArgs e)
+        {
+            if (e.Exception != null)
+            {
+                HandleAppException(e.Exception);
+            }
+
+            e.Handled = true;
+        }
+
+        private void AppDomainUnhandledException(object sender, UnhandledExceptionEventArgs e)
+        {
+            if (e.ExceptionObject is Exception exception)
+            {
+                HandleAppException(exception);
+            }
+        }
+
+        private void Initialize()
+        {
+            ComponentInitialize();
+            //HasBeenInitialized = true;
+        }
+
+        private void EnsureSingleInstance()
+        {
+            //Guid s = LogToHub("Checking duplicated instances");
+            //if (!CommonServicesModule.EnsureSingleInstance())
+            //{
+            //    if (WpfCommon.ShowMessageBox(
+            //        $"{CommonServicesModule.ApplicationName} is already running.{Environment.NewLine}Do you want to close it?",
+            //        buttons: MessageBoxButton.YesNo) == MessageBoxResult.No)
+            //    {
+            //        Environment.Exit(0);
+            //    }
+            //    else
+            //    {
+            //        foreach (int pid in CommonServicesModule.GetOtherInstances())
+            //        {
+            //            using (var p = Process.GetProcessById(pid))
+            //            {
+            //                p.Kill();
+            //            }
+            //        }
+            //    }
+            //}
+            //RemoveLog(s);
         }
     }
 }

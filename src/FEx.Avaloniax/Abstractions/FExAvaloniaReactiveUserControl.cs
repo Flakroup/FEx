c912e031d0b77a88708bdaca1712aed9b836ad10
Jan Abramek <jan.abramek@tasksoftware.com>
2023-12-27 00:02:25 +0100

    Added Avalonia base classes and services

---
+++ src/FEx.Avaloniax/Abstractions/FExAvaloniaReactiveUserControl.cs
@@ -0,0 +1,57 @@
+using Avalonia.ReactiveUI;
+using ReactiveUI;
+using System;
+using System.Reactive.Disposables;
+
+namespace FEx.Avaloniax.Abstractions;
+
+public abstract class FExAvaloniaReactiveUserControl<T> : ReactiveUserControl<T>, IDisposable
+    where T : class, IActivatableViewModel
+{
+    protected readonly CompositeDisposable _disposables;
+    protected bool _disposed;
+
+    protected FExAvaloniaReactiveUserControl()
+    {
+        _disposables = [];
+    }
+
+    protected virtual void OnActivated()
+    {
+    }
+
+    protected virtual void OnDeactivated()
+    {
+    }
+
+    protected void SubscribeToWhenActivated()
+    {
+        IDisposable subscription = this.WhenActivated(disposables =>
+        {
+            OnActivated();
+            Disposable.Create(OnDeactivated).DisposeWith(disposables);
+        });
+
+        _disposables.Add(subscription);
+    }
+
+    #region IDisposable
+    public virtual void Dispose()
+    {
+        if (_disposed)
+            return;
+
+        _disposed = true;
+        _disposables.Dispose();
+        GC.SuppressFinalize(this);
+    }
+
+    protected virtual void ThrowIfDisposed() =>
+#if NETSTANDARD
+        if (_disposed)
+            throw new ObjectDisposedException(GetType().FullName);
+#else
+        ObjectDisposedException.ThrowIf(_disposed, this);
+#endif
+    #endregion
+}

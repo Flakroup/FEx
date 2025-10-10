901a377b0472fb63e87f769613aff081e8664702
Jan Abramek <abramek.j@gmail.com>
2025-10-10 12:13:16 +0200

    Migrated Rest

---
+++ src/FEx.Json/Extensions/NotifyPropertyChangedExtensions.cs
@@ -0,0 +1,20 @@
+﻿using FEx.Agnostics.Abstractions.Interfaces;
+using FEx.Json.Extensions;
+using Newtonsoft.Json;
+using System;
+using System.Runtime.CompilerServices;
+
+namespace FEx.Json.Extensions;
+
+public static class NotifyPropertyChangedExtensions
+{
+    public static bool SetPropertyAndDeserializeIt<TEntity>(this IFExNotifyPropertyChanged obj,
+                                                            ref string backingField,
+                                                            string jsonValue,
+                                                            Action<TEntity> onDeserializedAction = null,
+                                                            JsonSerializerSettings settings = null,
+                                                            [CallerMemberName] string propertyName = null) =>
+        obj.SetProperty(ref backingField, jsonValue.TrimJsonString(), onDeserializedAction is not null
+            ? v => onDeserializedAction(v.FromJson<TEntity>(settings))
+            : null, propertyName);
+}

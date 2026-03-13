ed9bc05d7bdbd0f040792cb4790203e3e523115e
Jan Abramek <abramek.j@gmail.com>
2026-03-13 20:14:04 +0100

    Add ITestTarget interface to FEx.Building
    Reusable NUKE target for running dotnet test with TRX logger.
    Output goes to artifacts/test-results/. FEx Build.cs now uses
    ITestTarget instead of inline Test target.

---
+++ src/FEx.Building/ITestTarget.cs
@@ -0,0 +1,26 @@
+using Nuke.Common;
+using Nuke.Common.IO;
+using Nuke.Common.Tools.DotNet;
+
+using static Nuke.Common.Tools.DotNet.DotNetTasks;
+
+namespace FEx.Building;
+
+public interface ITestTarget : INukeBuild
+{
+    sealed AbsolutePath TestResultsDirectory => NukeBuild.RootDirectory / "artifacts" / "test-results";
+
+    Target Test => _ => _
+        .Description("Runs tests with TRX logger")
+        .Executes(() =>
+        {
+            TestResultsDirectory.CreateOrCleanDirectory();
+
+            DotNetTest(s => s
+                .SetProjectFile(((FExBuild)this).Solution)
+                .SetConfiguration(((FExBuild)this).Configuration)
+                .EnableNoBuild()
+                .SetResultsDirectory(TestResultsDirectory)
+                .SetLoggers("trx"));
+        });
+}

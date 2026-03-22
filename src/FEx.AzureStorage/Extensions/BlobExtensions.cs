a720c6a7ba6949de418c7dc5b70d98becccc123c
Jan Abramek <abramek.j@gmail.com>
2026-03-22 01:21:04 +0100

    S2360: Replace optional parameters with overloads in AzureStorage
    IAzureStorageService: extension methods (AzureStorageServiceExtensions.cs)
    AzureStorageService: remove defaults from all 14 methods, fix internal callers
    CloudBlockBlobInfo: constructor overload, EnsureExistsAsync/FetchAttributesAsync overloads
    BlobExtensions: ListBlobsAsync overload
    ProgressState: constructor overload, Reset overload

--- src/FEx.AzureStorage/Extensions/BlobExtensions.cs
+++ src/FEx.AzureStorage/Extensions/BlobExtensions.cs
@@ -69,14 +69,20 @@ public static class BlobExtensions
         return results;
     }
 
+    public static Task<Result<IList<IListBlobItem>, StackError>> ListBlobsAsync(
+        this CloudBlobContainer client,
+        string prefix,
+        CancellationToken cancellationToken) =>
+        client.ListBlobsAsync(prefix, cancellationToken, false, BlobListingDetails.None, null, null);
+
     public static async Task<Result<IList<IListBlobItem>, StackError>> ListBlobsAsync(
         this CloudBlobContainer client,
         string prefix,
         CancellationToken cancellationToken,
-        bool useFlatBlobListing = false,
-        BlobListingDetails blobListingDetails = BlobListingDetails.None,
-        BlobRequestOptions options = null,
-        OperationContext operationContext = null)
+        bool useFlatBlobListing,
+        BlobListingDetails blobListingDetails,
+        BlobRequestOptions options,
+        OperationContext operationContext)
     {
         if (prefix.IsNotNullOrEmptyString())
         {

7d770b4cec772e8046f3f4566f94c8771da13a12
Jan Abramek <abramek.j@gmail.com>
2026-06-23 13:40:45 +0200

    FEx.AzureStorage: add StrongInject FExAzureStorageModule
    Registers AzureStorageService as IAzureStorageService and exposes IFExAzureStorageContainer so downstream StrongInject containers consume Azure storage via the interface.

---
+++ src/FEx.AzureStorage/FExAzureStorageModule.cs
@@ -0,0 +1,13 @@
+﻿using StrongInject;
+
+namespace FEx.AzureStorage
+{
+    [Register(typeof(AzureStorageService), Scope.SingleInstance, typeof(IAzureStorageService))]
+    public class FExAzureStorageModule
+    {
+    }
+
+    public interface IFExAzureStorageContainer : IContainer<IAzureStorageService>
+    {
+    }
+}

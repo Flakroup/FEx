41c015e25521108024caad7e0ccb2290bf529973
Jan Abramek <abramek.j@gmail.com>
2026-07-28 12:08:48 +0200

    Point the BehaviorSubject doc references at the types that declare them
    ReSharper cannot resolve a cref to an inherited member of BehaviorSubject<T>, and an unresolved cref makes
    it stop counting usages in the whole file - so every using in these two files was reported as redundant,
    including three the compiler proves are used. Measured: swapping only the crefs drops the reported count
    from 6 to 2, while removing any of those three fails the build (CS0246/CS0103).
    
    Retargeting to IObserver<T>/IDisposable, which actually declare OnNext/OnError/OnCompleted/Dispose, keeps
    the documentation saying the same thing and makes it resolve for ReSharper as well as the compiler. That
    leaves System.Reactive.Subjects genuinely unused in the interface, so it goes.

--- src/Core.Abstractions/Interfaces/IFExBehaviorSubject.cs
+++ src/Core.Abstractions/Interfaces/IFExBehaviorSubject.cs
@@ -1,5 +1,4 @@
 using System;
-using System.Reactive.Subjects;
 
 namespace FEx.Core.Abstractions.Interfaces;
 
@@ -9,19 +8,19 @@ public interface IFExBehaviorSubject<T> : IFExSubject<T>
     /// Gets the current value or throws an exception.
     /// </summary>
     /// <value>
-    /// The initial value passed to the constructor until <see cref="BehaviorSubject{T}.OnNext" /> is called; after
-    /// which, the last value passed to <see cref="BehaviorSubject{T}.OnNext" />.
+    /// The initial value passed to the constructor until <see cref="IObserver{T}.OnNext(T)" /> is called; after
+    /// which, the last value passed to <see cref="IObserver{T}.OnNext(T)" />.
     /// </value>
     /// <remarks>
-    ///     <para><see cref="Value" /> is frozen after <see cref="BehaviorSubject{T}.OnCompleted" /> is called.</para>
+    ///     <para><see cref="Value" /> is frozen after <see cref="IObserver{T}.OnCompleted()" /> is called.</para>
     ///     <para>
-    ///     After <see cref="BehaviorSubject{T}.OnError" /> is called, <see cref="Value" /> always throws the specified
+    ///     After <see cref="IObserver{T}.OnError(System.Exception)" /> is called, <see cref="Value" /> always throws the specified
     ///     exception.
     ///     </para>
-    ///     <para>An exception is always thrown after <see cref="BehaviorSubject{T}.Dispose" /> is called.</para>
+    ///     <para>An exception is always thrown after <see cref="IDisposable.Dispose()" /> is called.</para>
     ///     <alert type="caller">
     ///     Reading <see cref="Value" /> is a thread-safe operation, though there's a potential race condition when
-    ///     <see cref="BehaviorSubject{T}.OnNext" /> or <see cref="BehaviorSubject{T}.OnError" /> are being invoked
+    ///     <see cref="IObserver{T}.OnNext(T)" /> or <see cref="IObserver{T}.OnError(System.Exception)" /> are being invoked
     ///     concurrently.
     ///     In some cases, it may be necessary for a caller to use external synchronization to avoid race conditions.
     ///     </alert>

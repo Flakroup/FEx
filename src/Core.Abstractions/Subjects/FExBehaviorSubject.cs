07a6d475c50a6e1856ea99ddad9cf7fc91b8a58b
Jan Abramek <abramek.j@gmail.com>
2026-07-28 13:08:58 +0200

    Drop the redundant System qualifier from the OnError doc references
    Both files already have `using System;`, so the cref resolves without spelling out the namespace - and a
    fully-qualified type name is what the repo convention tells you not to write. Follow-up to #30, which
    introduced the qualifier.

--- src/Core.Abstractions/Subjects/FExBehaviorSubject.cs
+++ src/Core.Abstractions/Subjects/FExBehaviorSubject.cs
@@ -21,13 +21,13 @@ public class FExBehaviorSubject<T> : FExSubject<T>, IFExBehaviorSubject<T>
     /// <remarks>
     ///     <para><see cref="Value" /> is frozen after <see cref="IObserver{T}.OnCompleted()" /> is called.</para>
     ///     <para>
-    ///     After <see cref="IObserver{T}.OnError(System.Exception)" /> is called, <see cref="Value" /> always throws the specified
+    ///     After <see cref="IObserver{T}.OnError(Exception)" /> is called, <see cref="Value" /> always throws the specified
     ///     exception.
     ///     </para>
     ///     <para>An exception is always thrown after <see cref="IDisposable.Dispose()" /> is called.</para>
     ///     <alert type="caller">
     ///     Reading <see cref="Value" /> is a thread-safe operation, though there's a potential race condition when
-    ///     <see cref="IObserver{T}.OnNext(T)" /> or <see cref="IObserver{T}.OnError(System.Exception)" /> are being invoked
+    ///     <see cref="IObserver{T}.OnNext(T)" /> or <see cref="IObserver{T}.OnError(Exception)" /> are being invoked
     ///     concurrently.
     ///     In some cases, it may be necessary for a caller to use external synchronization to avoid race conditions.
     ///     </alert>
